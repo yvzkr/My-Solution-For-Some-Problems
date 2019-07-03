d89c7f2d51ba55689cac700a34fcf1c198d2c9c4
yvzkr <yavuzkuru3@gmail.com>
2019-07-03 22:29:41 +0300

    new problem and solution

---
+++ diagonalDifference.py
@@ -0,0 +1,101 @@
+"""
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix  is shown below:
+
+1 2 3
+4 5 6
+9 8 9
+The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+
+Function description
+
+Complete the  function in the editor below. It must return an integer representing the absolute diagonal difference.
+
+diagonalDifference takes the following parameter:
+
+arr: an array of integers .
+Input Format
+
+The first line contains a single integer, , the number of rows and columns in the matrix .
+Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+Constraints
+
+Output Format
+
+Print the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+Sample Input
+
+3
+11 2 4
+4 5 6
+10 8 -12
+Sample Output
+
+15
+Explanation
+
+The primary diagonal is:
+
+11
+   5
+     -12
+Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+The secondary diagonal is:
+
+     4
+   5
+10
+Sum across the secondary diagonal: 4 + 5 + 10 = 19
+Difference: |4 - 19| = 15
+
+Note: |x| is the absolute value of x
+
+
+"""
+
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    n=len(arr)
+    return abs(sum(arr[i][n-i-1] for i in range(n))- sum(arr[i][i] for i in range(n)) )
+
+
+# Write your code here
+
+n = int(input().strip())
+
+arr = []
+
+"""for _ in range(n):
+    arr.append(list(map(int, input().rstrip().split())))
+print(arr)"""
+
+yavuz=[
+[11, 2, 4],
+[4, 5, 6],
+[10 ,8 ,-12]
+]
+
+result = diagonalDifference(yavuz)
+
+print(str(result) + '\n')
+
+
